324d6cbcbffd07b7c8514882f7ccefe7383dfd51
Brendan Allan <git@brendonovich.dev>
2026-03-17 13:36:31 +0800

    remove unneeded predev

--- packages/desktop-electron/scripts/predev.ts
+++ packages/desktop-electron/scripts/predev.ts
@@ -2,16 +2,4 @@ import { $ } from "bun"
 
 await $`bun ./scripts/copy-icons.ts ${process.env.OPENCODE_CHANNEL ?? "dev"}`
 
-// const RUST_TARGET = Bun.env.RUST_TARGET
-
-// const sidecarConfig = getCurrentSidecar(RUST_TARGET)
-
-// const binaryPath = windowsify(`../opencode/dist/${sidecarConfig.ocBinary}/bin/opencode`)
-
-// await (sidecarConfig.ocBinary.includes("-baseline")
-//   ? $`cd ../opencode && bun run build --single --baseline`
-//   : $`cd ../opencode && bun run build --single`)
-
 await $`cd ../opencode && bun script/build-node.ts`
-
-// await copyBinaryToSidecarFolder(binaryPath, RUST_TARGET)
